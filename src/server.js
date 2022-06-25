885c971dfa51bc48167296c9cd6786326777c6fd
al fahim bin faruk <fmd03700@gmail.com>
2022-06-26 01:38:13 +0600

    cors added

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const createError = require("http-errors");
+const cors = require("cors");
 const morgan = require("morgan");
 require("dotenv").config();
 
@@ -9,8 +10,15 @@ const app = express();
 //initialize DB
 connectDB();
 
+//provide cors options to only allow a specific frontend ip to connect to backend
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 //initialize common middlewares
 app.use([
+  cors(corsOptions),
   express.json(),
   express.urlencoded({ extended: false }),
   morgan("dev"),
